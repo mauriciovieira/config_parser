d6145f2fbbe2a17b27824e60545cb244580d4ed3
Maurício Vieira <mauricio@mauriciovieira.net>
2021-03-28 07:50:16 -0300

    chore: root
    bundle g config_parser -b

---
+++ lib/config_parser.rb
@@ -0,0 +1,6 @@
+require "config_parser/version"
+
+module ConfigParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/config_parser_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ConfigParser do
+  it "has a version number" do
+    expect(ConfigParser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
